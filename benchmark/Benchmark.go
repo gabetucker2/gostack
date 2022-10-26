e896c24e7bbfa5b18c3882d4944f16285a37f18f
gabetucker2 <gabe@the-tuckers.com>
2022-10-26 00:20:01 -0400

    Updated Benchmark

---
+++ benchmark/Benchmark.go
@@ -0,0 +1,9 @@
+package benchmark
+
+import (
+	"fmt"
+)
+
+func Run() {
+	fmt.Println(" - BEGINNING BENCHMARKS (fix errors in descending order)")
+}
--- executive/executive.go
+++ executive/executive.go
@@ -3,18 +3,15 @@ package main
 import (
 	"github.com/gabetucker2/gogenerics"
 	"github.com/gabetucker2/gostack/testing"
+	"github.com/gabetucker2/gostack/benchmark"
 )
 
 func main() {
 
 	// remove import errors when stuff is commented out
-	gogenerics.RemoveUnusedError(testing.Run)
+	gogenerics.RemoveUnusedError(testing.Run, benchmark.Run)
 
-	// tests
 	testing.Run(true)
-
-	// tutorials
-	// tutorials.Bootstrap()
-	// tutorials.Lambda()
+	benchmark.Run()
 
 }
