e2572ffbd380fe383ab7452e19cbd9004d4d2680
gabetucker2 <gabe@the-tuckers.com>
2023-01-31 07:43:45 -0500

    Fixed documentational innacuracy

--- Library.go
+++ Library.go
@@ -398,8 +398,8 @@ func (stack *Stack) DimensionalityReduce(arguments ...any) *Stack {
  stack.ToArray(returnType RETURN [RETURN_Vals]) (newArray []any)
 
  @examples
- | MakeStack([]int {1, 2, 3}, []string {"a", "b", "c"}).ToArray() => []any {1, 2, 3}
- | MakeStack([]int {1, 2, 3}, []string {"a", "b", "c"}).ToArray(RETURN_Keys) => []any {"a", "b", "c"}
+ | MakeStack([]int {1, 2, 3}, []string {"a", "b", "c"}).ToArray() => []any {"a", "b", "c"}
+ | MakeStack([]int {1, 2, 3}, []string {"a", "b", "c"}).ToArray(RETURN_Keys) => []any {1, 2, 3}
  | MakeStack([]int {1, 2, 3}, []string {"a", "b", "c"}).ToArray(RETURN_Idxs) => []any {0, 1, 2}
  | MakeStack([]*Card {cardA, cardB, cardC}).ToArray(RETURN_Cards) => []any {cardA, cardB, cardC}
  | MakeStack([]*Stack {substackA, substackB}).ToArray(RETURN_Cards) => []any {Card{Val:substackA}, Card{Val:substackA}}
